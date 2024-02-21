bf865eee34635c90addd7838e381790655bacc75
LorisSigrist <loris@sigrist.dev>
2024-02-21 15:19:53 +0100

    Split

--- src/hooks.js
+++ src/hooks.js
@@ -2,4 +2,8 @@ export function reroute({ url }) {
     if (url.pathname === "/some-page") {
         return "/"
     }
+
+    if(url.pathname === "/some-other-page") {
+        return "/page-1"
+    }
 }
